12a6240905f206e38ee04d2063432415713a2f2c
Edwin-1012 <bttagaredwin@gmail.com>
2021-06-09 14:15:32 -0500

    Subiendo los primeros archivos

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('URL','https://localhost/practicas/Cafehuertomamey/');
+define('HOST', 'localhost:3307');
+define('DB', 'bdcafetienda');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+?>
